fafa162d2fe3c9cc526f30ee01e6219158402075
baseproduct <123001159+baseproduct@users.noreply.github.com>
2023-02-05 12:30:31 -0600

    Create variables.kt

---
+++ Week02/variables.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var myAge: Int = 41
+    var averageAge: Double = (myAge + 30) / 2
+    println("In case you wanted to see this number...")
+    print(averageAge)
+    val testNumber = 23
+    val evenOdd = testNumber mod 2
+    println("I notice that evenOdd can only equal 0 or 1.")
+    var answer = 0
+    var answer = answer + 1
+    var answer = answer + 10
+    var answer = answer * 10
+    var answer = answer * 1000
+    println("The answer is...")
+    print(answer)
+}
