b59621d0365cafd75c199b882608fe9a0da3cd6a
baseproduct <123001159+baseproduct@users.noreply.github.com>
2023-02-12 23:42:37 -0600

    Create star.kt

---
+++ Week02/star.kt
@@ -0,0 +1,12 @@
+fun main {
+	var string: line1 = "   *"
+	var string: line2 = "  ***"
+	var string: line3 = " *****"
+	print line1
+	print line2
+	print line3
+	println("*******")
+	print line3
+	print line2
+	print line1
+			}
