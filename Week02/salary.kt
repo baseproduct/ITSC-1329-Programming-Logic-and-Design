2876e504a121547a2a164dda7eeeee20ca356981
baseproduct <123001159+baseproduct@users.noreply.github.com>
2023-02-12 23:34:02 -0600

    Create salary.kt

---
+++ Week02/salary.kt
@@ -0,0 +1,6 @@
+fun main {
+	var string: name = Brett
+	var int: age = 41
+	var double: annualIncome = $175000
+	println("My name is $name. My age is $age. I hope to make $annualIncome per year.")
+		}
